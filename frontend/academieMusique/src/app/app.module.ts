25ce5c1c1630eb7489341c560afabbb79b1d0b93
CedricOrtega <cedric.ortega06210@gmail.com>
2020-04-10 14:27:16 +0200

    Mise en place environnemen dev et de l'auth -- prochaine etape le header qui comoortera la navigation

---
+++ frontend/academieMusique/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'auth/signup', component: SignupComponent},
+  {path: 'auth/login', component: LoginComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    AuthService,
+    AuthGuardService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
