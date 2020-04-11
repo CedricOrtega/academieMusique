03d7846c340d4b5105d838b7d5067c6c00186cf0
CedricOrtega <cedric.ortega06210@gmail.com>
2020-04-11 17:39:50 +0200

    Gestion état de l'auth (connexion/deconnexion)

--- frontend/academieMusique/src/app/main-nav/main-nav.component.ts
+++ frontend/academieMusique/src/app/main-nav/main-nav.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -10,12 +12,33 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
+  public isAuth: boolean;
+
+  private isAuthSub: Subscription;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private auth: AuthService, private router: Router) {}
+
+  ngOnInit(){
+    this.isAuthSub = this.auth.isAuth$.subscribe(
+      (auth) => {
+        this.isAuth = auth;
+      }
+    );
+  }
+
+  onLogout() {
+    this.auth.logout();
+    this.router.navigate(['/']);
+  }
+
+  ngOnDestroy() {
+    this.isAuthSub.unsubscribe();
+  }
 
 }
